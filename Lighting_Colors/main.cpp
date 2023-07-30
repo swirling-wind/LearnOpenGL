55d21ed7836c7d7cbb41af7d32a1023e2c2e480f
Oya <yaozirui@outlook.com>
2023-07-30 14:17:47 +0800

    Reformat main.cpp from Lighting::Colors

--- Lighting_Colors/main.cpp
+++ Lighting_Colors/main.cpp
@@ -48,8 +48,7 @@ unsigned int BuildShaderProgram()
 int main()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	SetCallbackAndLoadGlad(window);
@@ -69,19 +68,18 @@ int main()
 	};
 	glm::mat4 model_matrix = glm::mat4(1.0f), view_matrix = glm::mat4(1.0f), projection_matrix = glm::mat4(1.0f);
 	const unsigned int shader_program = BuildShaderProgram();
-	auto [vao, vbo, ebo] = GetVaoVboEbo();
-	auto textures_id = LoadTexture(shader_program);
+	const auto [vao, vbo, ebo] = GetVaoVboEbo();
+	const auto& [container_tex, face_tex] = LoadTexture(shader_program);
 	float delta_time_between_frames = 0.0f, last_frame_time = 0.0f;
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, container_tex);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, face_tex);	
 	while (!glfwWindowShouldClose(window))
 	{
-		const auto& [container_tex, face_tex] = textures_id;
 		glClearColor(0.2f, 0.3f, 0.3f, 0.2f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glUseProgram(shader_program);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, container_tex);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, face_tex);
+		glUseProgram(shader_program);		
 		glBindVertexArray(vao);
 
 		float current_frame_time = static_cast<float>(glfwGetTime());
@@ -90,16 +88,13 @@ int main()
 
 		view_matrix = camera.GetViewMatrix();
 		projection_matrix = glm::perspective(glm::radians(camera.zoom), 800.0f / 600.0f, 0.1f, 100.0f);
-		int view_location = glGetUniformLocation(shader_program, "view");
-		glUniformMatrix4fv(view_location, 1, GL_FALSE, glm::value_ptr(view_matrix));
-		int projection_location = glGetUniformLocation(shader_program, "projection");
-		glUniformMatrix4fv(projection_location, 1, GL_FALSE, glm::value_ptr(projection_matrix));
+		glUniformMatrix4fv(glGetUniformLocation(shader_program, "view"), 1, GL_FALSE, glm::value_ptr(view_matrix));
+		glUniformMatrix4fv(glGetUniformLocation(shader_program, "projection"), 1, GL_FALSE, glm::value_ptr(projection_matrix));
 		int model_location = glGetUniformLocation(shader_program, "model");
 		for (int i = 0; i < 10; ++i)
 		{
-			float angle = 20.0f * i;
 			model_matrix = glm::translate(glm::mat4(1.0f), cube_positions[i]);
-			model_matrix = glm::rotate(model_matrix, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			model_matrix = glm::rotate(model_matrix, glm::radians(20.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
 			if ((i + 1) % 3 == 0)
 			{
 				model_matrix = glm::rotate(model_matrix, current_frame_time * 0.2f, glm::vec3(1.0f, 0.3f, 0.5f));
