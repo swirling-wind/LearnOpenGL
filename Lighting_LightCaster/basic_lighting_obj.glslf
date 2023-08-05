d1420ecdbfce9ca02cc9a60c3ed7a38bd33f33d9
Oya <yaozirui@outlook.com>
2023-08-05 22:55:09 +0800

    feat (Lighting::LightCaster): Add a flash light cast from the camera

--- Lighting_LightCaster/basic_lighting_obj.glslf
+++ Lighting_LightCaster/basic_lighting_obj.glslf
@@ -1,4 +1,5 @@
 #version 330 core
+out vec4 FragColor;
 
 struct Material {
     sampler2D diffuse;
@@ -6,17 +7,21 @@ struct Material {
     float shininess;
 };
 struct Light {
+    vec3 position;
     vec3 direction;
+    float cutOff;
 
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 uniform Material material;
 uniform Light light;
-
-out vec4 FragColor;
 
 in vec3 Normal;  
 in vec3 FragPos;
@@ -26,20 +31,40 @@ uniform vec3 viewPos;
 
 void main()
 {
-    // ambient
-    vec3 ambient = vec3(texture(material.diffuse, TexCoords)) * light.ambient;
-  	
-    // diffuse 
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(-light.direction);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * vec3(texture(material.diffuse, TexCoords)) * light.diffuse;
-    
-    // specular
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = spec * light.specular * vec3(texture(material.specular, TexCoords));  
+    vec3 lightDir = normalize(light.position - FragPos);
+    float theta = dot(lightDir, normalize(-light.direction));
+
+    // flashlight
+    if (theta > light.cutOff) // within cosine
+    {
+        // ambient
+        vec3 ambient = vec3(texture(material.diffuse, TexCoords)) * light.ambient;
         
-    FragColor = vec4(ambient + diffuse + specular, 1.0);
+        // diffuse 
+        vec3 norm = normalize(Normal);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = diff * vec3(texture(material.diffuse, TexCoords)) * light.diffuse;
+        
+        // specular
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);  
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        vec3 specular = spec * light.specular * vec3(texture(material.specular, TexCoords));  
+            
+        // attenuation
+        float distance = length(light.position - FragPos);
+        float attenuation = 1.0 / 
+        (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+        
+        // ambient  *= attenuation; 
+        diffuse  *= attenuation;
+        specular *= attenuation;
+
+        FragColor = vec4(ambient + diffuse + specular, 1.0);
+    }
+    else
+    {
+        FragColor = vec4(light.ambient * texture(material.diffuse, TexCoords).rgb, 1.0);
+    }
+
 } 
--- Lighting_LightCaster/main.cpp
+++ Lighting_LightCaster/main.cpp
@@ -216,10 +216,10 @@ int main()
 		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
+	glm::vec3 lightPos(0.0f, 0.0f, -2.0f);
 	lightingShader.use();
 	lightingShader.setInt("material.diffuse", 0);
 	lightingShader.setInt("material.specular", 1);
-	//lightingShader.setInt("material.emission", 2);
 	
 	while (!glfwWindowShouldClose(window))
 	{
@@ -237,19 +237,26 @@ int main()
 		// ------
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
+
+		// be sure to activate shader when setting uniforms/drawing objects
 		lightingShader.use();
-		lightingShader.setVec3("material.specular", 0.9f, 0.9f, 0.9f);
-		lightingShader.setFloat("material.shininess", 256.0f);
-
-		lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-		lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-
-		float current_time = static_cast<float>(glfwGetTime());
-		lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+		lightingShader.setVec3("light.position", camera.Position);
+		lightingShader.setVec3("light.direction", camera.Front);
+		lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
 		lightingShader.setVec3("viewPos", camera.Position);
-		//lightingShader.setFloat("material.time_shift", current_time / 4.0f);
+
+		// light properties
+		lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+		// we configure the diffuse intensity slightly higher; the right lighting conditions differ with each lighting method and environment.
+		// each environment and lighting type requires some tweaking to get the best out of your environment.
+		lightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("light.constant", 1.0f);
+		lightingShader.setFloat("light.linear", 0.09f);
+		lightingShader.setFloat("light.quadratic", 0.032f);
+
+		// material properties
+		lightingShader.setFloat("material.shininess", 32.0f);
 
 		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -258,35 +265,49 @@ int main()
 		lightingShader.setMat4("view", view);
 
 		// world transformation
+		glm::mat4 model = glm::mat4(1.0f);
+		lightingShader.setMat4("model", model);
+
+		// bind diffuse map
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, diffuseMap);
+		// bind specular map
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, specularMap);
+
+		// render containers
+		glBindVertexArray(cubeVAO);
 		for (unsigned int i = 0; i < 10; i++)
 		{
+			// calculate the model matrix for each object and pass it to shader before drawing
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
-			model = glm::rotate(model, glm::radians(i * 20.0f), glm::vec3(1.0, 1.0, 0.0));
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			lightingShader.setMat4("model", model);
-			// render the cube
-			glBindVertexArray(cubeVAO);
+
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
 
-		// also draw the lamp object
-		//glm::mat4 model = glm::mat4(1.0f);
-		//lightCubeShader.use();
-		//lightCubeShader.setMat4("projection", projection);
-		//lightCubeShader.setMat4("view", view);
-		//model = glm::mat4(1.0f);
-		//model = glm::translate(model, lightPos);
-		//model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-		//lightCubeShader.setMat4("model", model);
-		//glBindVertexArray(lightCubeVAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		// again, a lamp object is weird when we only have a spot light, don't render the light object
+		// lightCubeShader.use();
+		// lightCubeShader.setMat4("projection", projection);
+		// lightCubeShader.setMat4("view", view);
+		// model = glm::mat4(1.0f);
+		// model = glm::translate(model, lightPos);
+		// model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+		// lightCubeShader.setMat4("model", model);
+
+		// glBindVertexArray(lightCubeVAO);
+		// glDrawArrays(GL_TRIANGLES, 0, 36);
+
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
 	// optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
 	glDeleteVertexArrays(1, &cubeVAO);
