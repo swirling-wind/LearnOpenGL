d9fbbac6d8ba2cbe2ec969b3bdcf43b989312dfb
Oya <yaozirui@outlook.com>
2023-08-10 21:44:49 +0800

    feat (ModelLoading::Mesh): Add dependencies of Assimp

--- ModelLoading_Mesh/main.cpp
+++ ModelLoading_Mesh/main.cpp
@@ -4,6 +4,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include "mesh.h"
 #include <iostream>
 #include <string>
--- ModelLoading_Mesh/mesh.h
+++ ModelLoading_Mesh/mesh.h
@@ -45,7 +45,6 @@ public:
         /* Example code of shader
             uniform sampler2D texture_diffuse1;
             uniform sampler2D texture_diffuse2;
-            uniform sampler2D texture_diffuse3;
             uniform sampler2D texture_specular1;
             uniform sampler2D texture_specular2;*/
         unsigned int diffuseNr = 1;
